8525afb851824e255e4af6d89c05ca06f590adb2
Aniruddha <ruddha.mine@gmail.com>
2020-02-15 03:51:14 +0530

    Added JWT Verify and MongoDB

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
-require("dotenv");
+require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const MongoClient = require("mongodb").MongoClient;
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
-app.use(bodyParser.json());
+const client = new MongoClient(process.env.MONGOURL, {
+  useUnifiedTopology: true
+});
 
-let auth = function(req, res, next) {
-  if (req.query.q == "wrong") {
-    return res.sendStatus(403);
+client.connect(function(err) {
+  if (err) return console.log(err);
+  console.log("Connected successfully to MongoDB");
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let auth = async function(req, res, next) {
+  try {
+    let status = await jwt.verify(req.body.token, process.env.SECRET);
+    if (status == false) {
+      return res.sendStatus(403);
+    }
+    return next();
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(500);
   }
-  return next();
 };
 
 app.post("/incoming", async function(req, res) {
