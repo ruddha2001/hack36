b86a7ddf13120189ded7b83a8a70be145d30cad9
Aaishika S Bhattacharya <aaishikasb@users.noreply.github.com>
2020-02-15 18:12:51 +0530

    App Updated

---
+++ EagleApp/app/src/main/java/com/megahertz/eagleapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.tutorialkart.webviewexample
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView.loadUrl("https://www.google.com/")
+
+    }
+}
