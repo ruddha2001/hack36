b323b0dce22ea0c987d3e17a4650bb80231097b5
Aniruddha <ruddha.mine@gmail.com>
2020-02-15 05:35:46 +0530

    Fixed Conflict

--- server.js
+++ server.js
@@ -23,11 +23,14 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Express serves static content from Website
+app.use(express.static(__dirname + "/Website"));
+
 let auth = async function(req, res, next) {
   try {
     let status = await jwt.verify(req.body.token, process.env.SECRET);
     if (status == false) {
-      return res.sendStatus(403);
+      return res.sendFile(path.join(__dirname + "/Website/login.html"));
     }
     return next();
   } catch (err) {
@@ -39,6 +42,36 @@ let auth = async function(req, res, next) {
 app.post("/incoming", async function(req, res) {
   console.log(req.body.value);
   res.sendStatus(200);
+});
+
+app.post("/login", async function(req, res) {
+  let email = req.body.email;
+  let password = req.body.password;
+
+  let collection = client.db("eagleAssist").collection("users");
+
+  try {
+    let result = await collection.findOne({ email: email });
+    let status = await bcrypt.compare(password, result["password"]);
+    if (status) {
+      let token = jwt.sign(
+        {
+          exp: Math.floor(Date.now() / 1000) + 60 * 60,
+          data: "foobar",
+          issuer: "Eagle Assist"
+        },
+        process.env.SECRET
+      );
+      return res.send(token);
+    } else {
+      return res.sendFile(
+        path.join(__dirname + "/Website/login.html?status=false")
+      );
+    }
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(500);
+  }
 });
 
 //Homepage
